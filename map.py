9e9301a60e4a27a25203b40b1833d2e34ca4ba34
kofileong <kofileon@buffalo.edu>
2025-12-04 22:09:07 -0500

    Detects 'x' on the grid

--- map.py
+++ map.py
@@ -38,4 +38,6 @@ class Map:
         return (tile_x, tile_y) in self.obstacles
     
     def check_car(self, x, y):
-        return
+        tile_x = x // 50
+        tile_y = y // 50
+        return self.grid[tile_x][tile_y] == 'x'
