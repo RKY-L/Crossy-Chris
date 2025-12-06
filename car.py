028960fd784d66060a23a33c7143eae9f6402d9e
Akumaa10 <143619078+Akumaa10@users.noreply.github.com>
2025-12-06 00:57:17 -0500

    Car randomly spawns on all lanes. no different directions yet

--- car.py
+++ car.py
@@ -6,12 +6,17 @@ class Car:
         self.x = 650
         self.y = road * 50
         self.frame_count = 0
+        direction = 0
     
     def update(self, map):
         """Move the car by 50 pixels every 2 frames along the X axis."""
         self.frame_count += 1
-        if self.frame_count % 4 == 0:
+        if self.frame_count % 4 == 0  and self.direction == 0:
             new_x = self.x - 50
+            map.updatecarpos(self.y, self.x,new_x)
+            self.x = new_x
+        elif self.frame_count % 4 == 0 and self.direction == 1:
+            new_x = self.x + 50
             map.updatecarpos(self.y, self.x,new_x)
             self.x = new_x
             
