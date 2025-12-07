4f3bcb3963b276db5e18bf73065725298a614a76
Akumaa10 <143619078+Akumaa10@users.noreply.github.com>
2025-12-07 17:52:33 -0500

    added training

--- model.py
+++ model.py
@@ -31,3 +31,32 @@ class QTrainer:
         self.model = model
         self.optimizer = optim.Adam(model.parameters(), lr = self.lr)
         self.lossfunct = nn.MSELoss()
+
+    def train_step(self, state, action, reward, new_state, done):
+        state = torch.tensor(state, dtype = torch.float)
+        new_state = torch.tensor(new_state, dtype = torch.float)
+        action = torch.tensor(action, dtype = torch.long)
+        reward = torch.tensor(reward, dtype = torch.float)
+
+        if len(state.shape) == 1:
+            state = torch.unsqueeze(state, 0)
+            new_state = torch.unsqueeze(new_state, 0)
+            action = torch.unsqueeze(action,0)
+            reward = torch.unsqueeze(reward,0)
+            done = (done, )
+
+        pred = self.model(state)
+
+        target = pred.clone()
+        for idx in range(len(done)):
+            Q_new = reward[idx]
+            if not done[idx]:
+                #Update Q table from slides. 
+                Q_new = reward[idx] + self.gamma * torch.max(self.model(new_state[idx]))
+            target[idx][torch.argmax(action[idx]).item()] = Q_new
+        
+
+        self.optimizer.zero_grad()
+        loss = self.lossfunct(target, pred)
+        loss.backward()
+        self.optimizer.step()
